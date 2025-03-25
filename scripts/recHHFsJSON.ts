398ba124f367ca8914a7c06d0d42e85dc03b99ae
Howler Monkey <howlermonkey@uspsa.org>
2025-03-24 18:51:08 -0600

    megadoc approved, export for USPSA to implement

---
+++ scripts/recHHFsJSON.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+
+import { deprecatedUSPSAClassifiers } from "../api/src/dataUtil/classifiersData";
+import { Classifiers } from "../api/src/db/classifiers";
+import { connect } from "../api/src/db/index";
+import { uspsaDivShortNames } from "../shared/constants/divisions";
+
+const classifiersForDivision = async (division: string) =>
+  Classifiers.find({
+    division,
+    classifier: { $nin: deprecatedUSPSAClassifiers },
+  }).populate("recHHFs");
+
+const go = async () => {
+  await connect();
+
+  const allDivs = {};
+  for (const division of uspsaDivShortNames) {
+    if (division === "l10") {
+      continue;
+    }
+    allDivs[division] = {};
+    const classifiers = await classifiersForDivision(division);
+    classifiers.forEach(({ classifier, recHHFs: { recHHF } }) => {
+      allDivs[division][classifier] = Number(recHHF.toFixed(4));
+    });
+  }
+
+  console.log(JSON.stringify(allDivs, null, 2));
+
+  process.exit(0);
+};
+
+go();
